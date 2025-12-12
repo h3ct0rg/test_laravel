76ca94d3105c0f5ca3f2dd0bea3c7fcb001a4efe
EMshady92 <daniel24992@gmail.com>
2025-12-12 15:21:56 -0600

    Add factories and seeder for users and tasks demo data

--- test_laravel/app/Http/Controllers/Api/TaskController.php
+++ test_laravel/app/Http/Controllers/Api/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\TaskResource;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
