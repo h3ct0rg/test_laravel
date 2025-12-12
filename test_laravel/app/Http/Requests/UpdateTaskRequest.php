071a6c6bb78629b9239dc6f00a5037d1d4ef85f1
EMshady92 <daniel24992@gmail.com>
2025-12-12 14:40:43 -0600

    Add form requests for task validation

---
+++ test_laravel/app/Http/Requests/UpdateTaskRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'titulo'      => ['required', 'string', 'max:255'],
+            'descripcion' => ['nullable', 'string'],
+            'estado'      => ['required', 'in:pendiente,en_progreso,completada'],
+            'user_id'     => ['required', 'exists:users,id'],
+        ];
+    }
+}
