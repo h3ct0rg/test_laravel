9bc27f8d870af8c54ce0f8d736ab17023503ca08
EMshady92 <daniel24992@gmail.com>
2025-12-12 16:18:52 -0600

    Add Tailwind CSS integration for Laravel + Vue 2

--- test_laravel/resources/views/app.blade.php
+++ test_laravel/resources/views/app.blade.php
@@ -5,10 +5,10 @@
     <title>Tasks SPA</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
-<body>
-<div id="app">
+<body class="min-h-screen bg-slate-900 text-white">
+<div id="app" class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold mb-4">Tasks SPA</h1>
     <router-view></router-view>
 </div>
-<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
